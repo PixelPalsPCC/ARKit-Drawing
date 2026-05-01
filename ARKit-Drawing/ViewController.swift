b6353d14104028a5e97b342bd0f31abf4fdbdb84
GeorgeMcK <george@hookstream.com>
2026-05-01 15:48:28 -0700

    Initial commit

---
+++ ARKit-Drawing/ViewController.swift
@@ -0,0 +1,102 @@
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
+    enum ObjectPlacementMode {
+        case freeform, plane, image
+    }
+    
+    var objectMode: ObjectPlacementMode = .freeform
+    var selectedNode: SCNNode?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sceneView.delegate = self
+        sceneView.autoenablesDefaultLighting = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+
+    @IBAction func changeObjectMode(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            objectMode = .freeform
+        case 1:
+            objectMode = .plane
+        case 2:
+            objectMode = .image
+        default:
+            break
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showOptions" {
+            let optionsViewController = segue.destination as! OptionsContainerViewController
+            optionsViewController.delegate = self
+        }
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        guard let node = selectedNode, let touch = touches.first else { return }
+        
+        switch objectMode {
+            case .freeform:
+                addNodeInFront(node)
+            case .plane:
+                break
+            case .image:
+                break
+        }
+    }
+
+    func addNodeInFront(_ node: SCNNode) {
+        guard let currentFrame = sceneView.session.currentFrame else { return }
+        
+        // Set transform of node to be 20cm in front of camera
+        var translation = matrix_identity_float4x4
+        translation.columns.3.z = -0.2
+        node.simdTransform = matrix_multiply(currentFrame.camera.transform, translation)
+        
+        let cloneNode = node.clone()
+        sceneView.scene.rootNode.addChildNode(cloneNode)
+    }
+
+
+}
+
+extension ViewController: OptionsViewControllerDelegate {
+
+    func objectSelected(node: SCNNode) {
+        dismiss(animated: true, completion: nil)
+        selectedNode = node
+    }
+    
+    func togglePlaneVisualization() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func undoLastObject() {
+        
+    }
+    
+    func resetScene() {
+        dismiss(animated: true, completion: nil)
+    }
+}
